4775afc10274c037c43aca250ad13ff61959f63e
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-04 16:37:24 +0300

    Added Transaction model. Added Categories.

--- models/transaction-model.js
+++ models/transaction-model.js
@@ -1,1 +1,28 @@
-// Here comes transaction model
+const { Schema, model } = require('mongoose');
+const Categories = require('../helpers/categories');
+
+const transactionSchema = new Schema({
+  date: { type: String, required: [true, 'Date is required.'] },
+  income: {
+    type: Boolean,
+    required: [true, 'Income type is required.'],
+    default: false,
+  },
+  category: {
+    type: String,
+    required: [true, 'Category is required.'],
+    enum: Categories,
+    default: 'Основные расходы',
+  },
+  comment: { type: String },
+  sum: { type: Number, min: 0 },
+  balance: { type: Number, required: [true, 'Sum is required.'] },
+  owner: {
+    type: SchemaTypes.ObjectId,
+    ref: 'user',
+  },
+});
+
+const Transaction = model('transaction', transactionSchema);
+
+module.exports = Transaction;
