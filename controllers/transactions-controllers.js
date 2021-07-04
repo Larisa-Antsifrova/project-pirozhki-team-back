cd080329da677c7821daacfaceab3daf69619c68
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-04 18:15:48 +0300

    Added Transactions router.

--- controllers/transactions-controllers.js
+++ controllers/transactions-controllers.js
@@ -1,1 +1,17 @@
-// transactions endpoints handlers
+const getAllTransactions = (req, res, next) => {
+  try {
+    res.json({ message: 'Hello from all transactions' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const addTransaction = (req, res, next) => {
+  try {
+    res.json({ message: 'Hello from add new transactions' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getAllTransactions, addTransaction };
