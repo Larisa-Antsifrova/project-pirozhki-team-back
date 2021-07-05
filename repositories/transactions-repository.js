690803f1057e284e14b81dab4e4d3b7b304bfd42
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-05 12:22:29 +0300

    Refactored transactions repository to class.

--- repositories/transactions-repository.js
+++ repositories/transactions-repository.js
@@ -1,18 +1,17 @@
 const Transaction = require('../models/transaction-model');
 
-const getAllTransactions = async () => {
-  // TODO: Add search by owner ID
-  return await Transaction.find();
-};
+class Transactions {
+  async getAllTransactions() {
+    // TODO: Add search by owner ID
+    return await Transaction.find();
+  }
 
-const addTransaction = async transaction => {
-  // TODO: Add owner field with current user ID
-  return await Transaction.create({
-    ...transaction,
-  });
-};
+  async addTransaction(transaction) {
+    // TODO: Add owner field with current user ID
+    return await Transaction.create({
+      ...transaction,
+    });
+  }
+}
 
-module.exports = {
-  getAllTransactions,
-  addTransaction,
-};
+module.exports = new Transactions();
