6a5039e5b2f2dff5cdaa996224b3e08e2d8c6885
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-05 16:54:26 +0300

    Added statistics router, controllers.

--- router/statistics-routes.js
+++ router/statistics-routes.js
@@ -1,1 +1,8 @@
-// /statistics
+const { Router } = require('express');
+const Controllers = require('../controllers/statistics-controllers');
+
+const statisticsRoutes = Router();
+
+statisticsRoutes.get('/statistics', Controllers.getStatistics);
+
+module.exports = statisticsRoutes;
