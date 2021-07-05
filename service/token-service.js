534b8b483ac66116eaf7ff06c256e9265628f681
Anna Sokolova <sokolovaskorik@gmail.com>
2021-07-05 21:51:41 +0200

    Added token creation

---
+++ service/token-service.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const TokenModel = require("../models/token-model");
+
+const JWT_SECRET_KEY_ACCESS = process.env.JWT_SECRET_KEY_ACCESS;
+const JWT_SECRET_KEY_REFRESH = process.env.JWT_SECRET_KEY_REFRESH;
+
+class TokenService {
+  generateTokens(payload) {
+    const accessToken = jwt.sign(payload, JWT_SECRET_KEY_ACCESS, { expiresIn: "2h" });
+    const refreshToken = jwt.sign(payload, JWT_SECRET_KEY_REFRESH, { expiresIn: "30d" });
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+
+  async saveToken(userId, refreshToken) {
+    const tokenData = await TokenModel.findOne({ user: userId });
+    if (tokenData) {
+      tokenData.refreshToken = refreshToken;
+      return tokenData.save();
+    }
+
+    const token = await TokenModel.create({ user: userId, refreshToken });
+    return token;
+  }
+}
+
+module.exports = new TokenService();
