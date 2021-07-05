77293ba659cfed8ffb82efde3c8c2ddf8121caec
Anna Sokolova <sokolovaskorik@gmail.com>
2021-07-05 13:22:49 +0200

    Added template controllers for auth

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,1 +1,38 @@
 // Handlers for auth endpoints
+
+class AuthController {
+  //User registration
+  async registration(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  //User login
+  async login(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  //User logout
+  async logout(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  //Test
+  async getUsers(req, res, next) {
+    try {
+      res.json({ message: "Hello auth router" });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = new AuthController();
