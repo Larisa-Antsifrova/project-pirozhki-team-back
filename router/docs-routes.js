c7f4a1367bf72fde0524cf68be5c57fb5a276bb1
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-06 14:48:35 +0300

    Removed swagger header.

--- router/docs-routes.js
+++ router/docs-routes.js
@@ -4,7 +4,11 @@ const swaggerDocument = require('../swagger.json');
 
 const docsRoutes = Router();
 
+const options = {
+  customCss: '.swagger-ui .topbar { display: none }',
+};
+
 docsRoutes.use('/api-docs', swaggerUi.serve);
-docsRoutes.get('/api-docs', swaggerUi.setup(swaggerDocument));
+docsRoutes.get('/api-docs', swaggerUi.setup(swaggerDocument, options));
 
 module.exports = docsRoutes;
