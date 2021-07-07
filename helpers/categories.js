1af5d29baa937dacada42b05d86b82bf9086c3d1
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-08 00:04:36 +0300

    Added colors to categories.

--- helpers/categories.js
+++ helpers/categories.js
@@ -2,50 +2,62 @@ const Categories = [
   {
     name: 'Регулярный доход',
     income: true,
+    color: '#1b5e20',
   },
   {
     name: 'Нерегулярный доход',
     income: true,
+    color: '#4caf50',
   },
   {
     name: 'Разное',
     income: false,
+    color: '#b71c1c',
   },
   {
     name: 'Машина',
     income: false,
+    color: '#6e78e8',
   },
   {
     name: 'Продукты',
     income: false,
+    color: '#4a56e2',
   },
   {
     name: 'Забота о себе',
     income: false,
+    color: '#81e1ff',
   },
   {
     name: 'Забота о детях',
     income: false,
+    color: '#24cca7',
   },
   {
     name: 'Товары для дома',
     income: false,
+    color: '#00ad84',
   },
   {
     name: 'Образование',
     income: false,
+    color: '#c5baff',
   },
   {
     name: 'Досуг',
     income: false,
+    color: '#fd9498',
   },
   {
     name: 'Другие расходы',
     income: false,
+    color: '#ffd8d0',
   },
   {
     name: 'Основные расходы',
     income: false,
+    color: '#fed057',
   },
 ];
 
