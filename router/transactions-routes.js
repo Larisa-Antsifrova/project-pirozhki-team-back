f7cf8033aeed36442d4a2a82cbb0b62696873a2d
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-09 12:20:07 +0300

    Added auth middleware to transactions routes.

--- router/transactions-routes.js
+++ router/transactions-routes.js
@@ -1,16 +1,18 @@
 const { Router } = require('express');
+const guard = require('../middleware/auth-middleware');
 const {
-  validateCreatedTransaction,
+  validateCreatedTransaction
 } = require('../validaton/transactions-validation');
 const Controllers = require('../controllers/transactions-controllers');
 
 const transactionsRoutes = Router();
 
-transactionsRoutes.get('/transactions', Controllers.getAllTransactions);
+transactionsRoutes.get('/transactions', guard, Controllers.getAllTransactions);
 transactionsRoutes.post(
   '/transactions',
+  guard,
   validateCreatedTransaction,
-  Controllers.addTransaction,
+  Controllers.addTransaction
 );
 
 module.exports = transactionsRoutes;
