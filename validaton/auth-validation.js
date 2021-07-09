2f59fa4e9a95fdb684abc8566c41727c5a2f2dce
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-09 12:00:05 +0300

    Changed 20 to 12.

--- validaton/auth-validation.js
+++ validaton/auth-validation.js
@@ -1,10 +1,10 @@
-const Joi = require("joi");
-const HttpCodes = require("../helpers/http-codes");
+const Joi = require('joi');
+const HttpCodes = require('../helpers/http-codes');
 
 const schemaCreateUser = Joi.object({
-  password: Joi.string().trim().min(6).max(20).required(),
+  password: Joi.string().trim().min(6).max(12).required(),
   email: Joi.string().trim().email({ minDomainSegments: 2 }).required(),
-  name: Joi.string().trim().min(1).max(30).optional(),
+  name: Joi.string().trim().min(1).max(30).optional()
 });
 
 const validateSchema = async (schema, obj, next) => {
@@ -14,7 +14,7 @@ const validateSchema = async (schema, obj, next) => {
   } catch (err) {
     next({
       status: HttpCodes.BAD_REQUEST,
-      message: `${err.message.replace(/"/g, "")}`,
+      message: `${err.message.replace(/"/g, '')}`
     });
   }
 };
@@ -22,5 +22,5 @@ const validateSchema = async (schema, obj, next) => {
 module.exports = {
   schemaCreateUser: (req, _res, next) => {
     return validateSchema(schemaCreateUser, req.body, next);
-  },
+  }
 };
