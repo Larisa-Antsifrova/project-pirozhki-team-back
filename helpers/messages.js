062c0248c3fb8675081be50a90ddf6b37523856a
Anna Sokolova <sokolovaskorik@gmail.com>
2021-07-11 01:30:39 +0200

    Added the messages to helpers

---
+++ helpers/messages.js
@@ -0,0 +1,7 @@
+const Messages = {
+  CONFLICT_USER_AUTH: "The user with this email already exists.",
+  UNAUTHORIZED_USER_AUTH: "Invalid credentials.",
+  UNAUTHORIZED_TOKEN: "Not authorized.",
+};
+
+module.exports = Messages;
