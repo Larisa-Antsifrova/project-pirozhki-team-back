999b6a4a96809ec2567499e2cdfa838cd16441bd
Anna Sokolova <sokolovaskorik@gmail.com>
2021-07-12 11:10:13 +0200

    Added a NOT_FOUND_USER message

--- helpers/messages.js
+++ helpers/messages.js
@@ -3,6 +3,7 @@ const Messages = {
   CONFLICT_USER_AUTH: "The user with this email already exists.",
   UNAUTHORIZED_USER_AUTH: "Invalid credentials.",
   UNAUTHORIZED_TOKEN: "Not authorized.",
+  NOT_FOUND_USER: "User was not found.",
   NOT_FOUND_CATEGORY: "Category was not found.",
   DELETE_CATEGORY: "Category was successfully deleted.",
   NOT_FOUND_TRANSACTION: "Transaction was not found.",
