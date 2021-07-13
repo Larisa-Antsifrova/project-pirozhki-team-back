87a6f0ad1339bece6110204cd715b15082f63c60
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-14 00:17:26 +0300

    Added optional chaining operator.

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,23 +1,24 @@
-const tokenService = require("../services/token-service");
-const Users = require("../repositories/users-repository");
-const HttpCodes = require("../helpers/http-codes");
-const Statuses = require("../helpers/statuses");
-const Messages = require("../helpers/messages");
+const tokenService = require('../services/token-service');
+const Users = require('../repositories/users-repository');
+const HttpCodes = require('../helpers/http-codes');
+const Statuses = require('../helpers/statuses');
+const Messages = require('../helpers/messages');
 
 const guard = async (req, res, next) => {
   try {
     const headerAuth = req.headers.authorization;
 
-    const accessToken = headerAuth?.split(" ")[1];
+    const accessToken = headerAuth?.split(' ')[1];
 
     const payload = tokenService.validateAccessToken(accessToken);
-    const requestedUser = await Users.getUserById(payload.id);
+
+    const requestedUser = await Users.getUserById(payload?.id);
 
     if (!headerAuth || !accessToken || !requestedUser) {
       return res.status(HttpCodes.UNAUTHORIZED).json({
         status: Statuses.ERROR,
         code: HttpCodes.UNAUTHORIZED,
-        message: Messages.UNAUTHORIZED_TOKEN,
+        message: Messages.UNAUTHORIZED_TOKEN
       });
     }
 
