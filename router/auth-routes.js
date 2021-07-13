935fe59e85c15c53e7131c8c4918fd5a8975fd18
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-13 16:00:59 +0300

    Added verification endpoint.

--- router/auth-routes.js
+++ router/auth-routes.js
@@ -1,17 +1,18 @@
-const { Router } = require('express');
-const guard = require('../middleware/auth-middleware');
+const { Router } = require("express");
+const guard = require("../middleware/auth-middleware");
 const {
   validateCreatedUser,
-  validateLoggedinUser
-} = require('../validaton/auth-validation');
-const Controllers = require('../controllers/auth-controllers');
+  validateLoggedinUser,
+} = require("../validaton/auth-validation");
+const Controllers = require("../controllers/auth-controllers");
 
 const authRoutes = Router();
 
-authRoutes.post('/registration', validateCreatedUser, Controllers.registration);
-authRoutes.post('/login', validateLoggedinUser, Controllers.login);
-authRoutes.post('/logout', guard, Controllers.logout);
-authRoutes.get('/verify/:link', Controllers.verify);
+authRoutes.post("/registration", validateCreatedUser, Controllers.registration);
+authRoutes.post("/login", validateLoggedinUser, Controllers.login);
+authRoutes.post("/logout", guard, Controllers.logout);
 authRoutes.get("/refresh", guard, Controllers.refresh);
+
+authRoutes.get("/verify/:link", Controllers.verify);
 
 module.exports = authRoutes;
